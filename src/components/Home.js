1fec0859c22933e532309a9fea6b4184f3535256
Shenglu Zhang <747531141@qq.com>
2023-06-06 23:40:40 +1000

    porfolio first draft

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,251 @@
-import React from "react";
-import { Canvas } from "react-three-fiber";
-import { PerspectiveCamera, OrbitControls } from "@react-three/drei";
+import { Suspense, useRef, useMemo, useState, useEffect } from "react";
+import * as THREE from "three";
+import { Canvas, useFrame, useThree } from "react-three-fiber";
+import {
+  ScrollControls,
+  Scroll,
+  useScroll,
+  Image,
+  Preload,
+  Text,
+  OrbitControls,
+  GizmoHelper,
+  GizmoViewport,
+  TrackballControls,
+  Html,
+  CameraControls,
+} from "@react-three/drei";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import "./Home.css";
+import "./cyberpunk-2077.css";
+import "./cyberpunk.css";
+import { RGBA_ASTC_10x10_Format } from "three";
+import ManVanishing from "./ManVanishing";
 
 function Home() {
   return (
     <div className="home">
       <Canvas>
-        <PerspectiveCamera makeDefault position={[0, 0, 100]} />
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
-        <OrbitControls />
+        <Suspense fallback={null}>
+          <ScrollControls damping={4} pages={5}>
+            <Scroll>
+              <Header />
+            </Scroll>
+            <Scroll>
+              <Terminal />
+            </Scroll>
+            <Scroll>
+              <ManVanishing />
+            </Scroll>
+            <Scroll>
+              <Cube />
+            </Scroll>
+            <Scroll>
+              <Cloud radius={7} />
+            </Scroll>
+            {/* <Scroll>
+              <ImageGroup />
+            </Scroll> */}
+            <Scroll html>
+              <h1 className="cyber-h" style={{ top: "130vh" }}>
+                Skills
+              </h1>
+            </Scroll>
+          </ScrollControls>
+          <Preload />
+        </Suspense>
       </Canvas>
     </div>
   );
 }
 
 export default Home;
+function Terminal() {
+  return (
+    <Html>
+      <div className="terminal cyber-tile cyber-glitch">
+        <h1 className="cyber-h">About Me</h1>
+        {/* <p className="line1">name：章盛鹭 | zhang,sheng-lu</p> */}
+        <p className="line2">
+          A full stack developer, who is passionate about web development and
+          eveything about technology.
+        </p>
+        <p className="line3">
+          Scroll down to know more about me!
+          <span className="cursor3">_</span>
+        </p>
+      </div>
+    </Html>
+  );
+}
+function Header() {
+  return (
+    <Html>
+      <div className="header cyber-banner bg-purple">
+        <p className="title cyberpunk-font-og">Portfolio</p>
+        <div className="github-url">
+          <a href="https://github.com/TheCrowd">TheCrowd</a>
+          <img src="./fonts/github-mark-white.svg" alt="github" />
+        </div>
+      </div>
+    </Html>
+  );
+}
+// cube with image texture
+function Cube() {
+  const data = useScroll();
+  const mesh = useRef();
+  useFrame(() => {
+    mesh.current.rotation.x = data.range(0, 1 / 3) * Math.PI + Math.PI * 0.2;
+    mesh.current.rotation.y = data.range(0, 1 / 3) * Math.PI + Math.PI * 0.2;
+    mesh.current.material.opacity = data.range(0, 1 / 3);
+    mesh.current.position.z = data.range(0, 1 / 3) * 10;
+  });
+  const texture = useLoader(THREE.TextureLoader, "images/matrix.jpg");
+  return (
+    <mesh ref={mesh} position={[2.5, 0.5, 0]}>
+      <boxBufferGeometry attach="geometry" args={[3, 3, 3]} />
+      <meshBasicMaterial attach="material" map={texture} />
+    </mesh>
+  );
+}
+
+function Word({ children, ...props }) {
+  const color = new THREE.Color();
+  const colors = [
+    "white",
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "purple",
+    "pink",
+    "brown",
+    "grey",
+    "black",
+  ];
+  const fontProps = {
+    font: "fonts/Inter-Bold.woff",
+    fontSize: 0.7,
+    letterSpacing: -0.05,
+    lineHeight: 1,
+    "material-toneMapped": false,
+  };
+  const ref = useRef();
+  const [hovered, setHovered] = useState(false);
+  const over = (e) => (e.stopPropagation(), setHovered(true));
+  const out = () => setHovered(false);
+  // Change the mouse cursor on hover
+  useEffect(() => {
+    if (hovered) document.body.style.cursor = "pointer";
+    return () => (document.body.style.cursor = "auto");
+  }, [hovered]);
+  // Tie component to the render-loop
+  useFrame(({ camera }) => {
+    // Make text face the camera
+    ref.current.quaternion.copy(camera.quaternion);
+    // Animate font color
+    ref.current.material.color.lerp(
+      color.set(hovered ? "#fa2720" : "white"),
+      0.1
+    );
+  });
+  return (
+    <Text
+      ref={ref}
+      onPointerOver={over}
+      onPointerOut={out}
+      onClick={() => console.log("clicked")}
+      {...props}
+      {...fontProps}
+      children={children}
+      color={new THREE.Color(colors[Math.floor(Math.random() * colors.length)])}
+    />
+  );
+}
+
+function Cloud({ radius = 15 }) {
+  const { height, width } = useThree((state) => state.viewport);
+  const wordGroup = useRef();
+  const data = useScroll();
+  const calculatePositionInSphere = (spherical, i, offset = 0) => {
+    const phi = Math.PI * (Math.sqrt(5) - 1);
+    const theta = phi * i;
+    const y = 1 - (i / (count - 1)) * 2;
+    const sRadius = Math.sqrt(1 - y * y);
+    const x = sRadius * Math.cos(theta);
+    const z = sRadius * Math.sin(theta);
+    const sPhi = Math.atan2(z, x);
+    const sTheta = Math.atan2(y, x);
+    const vector = new THREE.Vector3().setFromSpherical(
+      spherical.set(radius, sPhi + offset, sTheta + offset)
+    );
+    vector.setY(vector.y - height * 2.5);
+    vector.setZ(vector.z - width / 2);
+    return vector;
+  };
+
+  useFrame(() => {
+    const offset = data.range(0, 1) * Math.PI * 0.4;
+    const spherical = new THREE.Spherical();
+    for (let i = 0; i < wordGroup.current.children.length; i++) {
+      const child = wordGroup.current.children[i];
+      const vector = calculatePositionInSphere(spherical, i, offset);
+      child.position.copy(vector);
+    }
+  });
+  // Create a count x count random words with spherical distribution
+  const WORDS = [
+    "Express",
+    "Confluence",
+    "Ruby",
+    "Javascript",
+    "AWS",
+    "Postgres",
+    "Serverless",
+    "GraphQL",
+    "NodeJS",
+    "Typescript",
+    "Docker",
+    "Pair Programming",
+    "Functional Programming",
+    "Agile",
+    "Scrum",
+    "Kanban",
+    "TDD",
+    "BDD",
+    "CI/CD",
+    "Git",
+    "Github",
+    "Gitlab",
+    "Jira",
+    "React",
+    "Slack",
+    "Microsoft Teams",
+    "VSCode",
+    "Vim",
+    "Linux",
+    "MacOS",
+    "Windows",
+    "Ubuntu",
+    "CloudFormation",
+    "CloudWatch",
+  ];
+  const count = WORDS.length;
+  const words = useMemo(() => {
+    const temp = [];
+    const spherical = new THREE.Spherical();
+    for (let i = 0; i < count; i++) {
+      temp.push([calculatePositionInSphere(spherical, i), WORDS[i]]);
+    }
+    return temp;
+  }, [radius]);
+  return (
+    <group ref={wordGroup}>
+      {words.map(([pos, word], index) => (
+        <Word key={index} position={pos} children={word} />
+      ))}
+    </group>
+  );
+}
---
+++ src/components/ManVanishing.js
@@ -0,0 +1,239 @@
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
+import { useEffect, useState, useRef } from "react";
+import * as THREE from "three";
+import { generateUUID } from "three/src/math/MathUtils";
+import { useHelper } from "@react-three/drei";
+import { useFrame } from "react-three-fiber";
+
+export default function ManVanishing() {
+  const [maleModel, setMaleModel] = useState();
+  const [femaleModel, setFemaleModel] = useState();
+  const [meshes, setMeshes] = useState([]);
+  const loader = new OBJLoader();
+  const clock = new THREE.Clock();
+
+  useFrame(() => {
+    let delta = 10 * clock.getDelta();
+    delta = delta < 2 ? delta : 2;
+
+    for (let j = 0; j < meshes.length; j++) {
+      const data = meshes[j];
+      const positions = data.positions;
+      const initialPositions = data.initialPositions;
+
+      const count = positions.count;
+
+      if (data.start > 0) {
+        data.start -= 1;
+      } else {
+        if (data.direction === 0) {
+          data.direction = -1;
+        }
+      }
+
+      for (let i = 0; i < count; i++) {
+        const px = positions.getX(i);
+        const py = positions.getY(i);
+        const pz = positions.getZ(i);
+
+        // falling down
+        if (data.direction < 0) {
+          if (py > 0) {
+            positions.setXYZ(
+              i,
+              px + 1.5 * (0.5 - Math.random()) * data.speed * delta,
+              py + 3.0 * (0.25 - Math.random()) * data.speed * delta,
+              pz + 1.5 * (0.5 - Math.random()) * data.speed * delta
+            );
+          } else {
+            data.verticesDown += 1;
+          }
+        }
+
+        // rising up
+        if (data.direction > 0) {
+          const ix = initialPositions.getX(i);
+          const iy = initialPositions.getY(i);
+          const iz = initialPositions.getZ(i);
+
+          const dx = Math.abs(px - ix);
+          const dy = Math.abs(py - iy);
+          const dz = Math.abs(pz - iz);
+
+          const d = dx + dy + dx;
+
+          if (d > 1) {
+            positions.setXYZ(
+              i,
+              px -
+                ((px - ix) / dx) * data.speed * delta * (0.85 - Math.random()),
+              py - ((py - iy) / dy) * data.speed * delta * (1 + Math.random()),
+              pz -
+                ((pz - iz) / dz) * data.speed * delta * (0.85 - Math.random())
+            );
+          } else {
+            data.verticesUp += 1;
+          }
+        }
+      }
+
+      // all vertices down
+      if (data.verticesDown >= count) {
+        if (data.delay <= 0) {
+          data.direction = 1;
+          data.speed = 5;
+          data.verticesDown = 0;
+          data.delay = 320;
+        } else {
+          data.delay -= 1;
+        }
+      }
+
+      // all vertices up
+      if (data.verticesUp >= count) {
+        if (data.delay <= 0) {
+          data.direction = -1;
+          data.speed = 15;
+          data.verticesUp = 0;
+          data.delay = 120;
+        } else {
+          data.delay -= 1;
+        }
+      }
+
+      positions.needsUpdate = true;
+    }
+  });
+  useEffect(() => {
+    if (maleModel && femaleModel) return;
+    loader.load("/models/male02.obj", (obj) => {
+      setMaleModel(obj);
+      const positions = combineBuffer(obj, "position");
+      const maleMesh = createMesh(positions, 0.01, -1, 0, 0, 0xff7744);
+      setMeshes([...meshes, ...maleMesh]);
+    });
+
+    loader.load("/models/female02.obj", (obj) => {
+      setFemaleModel(obj);
+      const positions = combineBuffer(obj, "position");
+      const femaleMeshes = createMesh(positions, 0.01, -1, 1, 0, 0xff7744);
+      setMeshes([...meshes, ...femaleMeshes]);
+    });
+  }, [maleModel, femaleModel]);
+  return <MeshGroup meshGroup={meshes} />;
+}
+
+function MeshGroup(props) {
+  const group = useRef();
+  useFrame(() => {
+    group.current.rotation.y += 0.0002;
+  });
+  const gridGeometry = new THREE.PlaneGeometry(15, 15, 64, 64);
+  const gridMaterial = new THREE.PointsMaterial({
+    size: 0.03,
+    color: "purple",
+  });
+  return (
+    <object3D
+      ref={group}
+      rotation={new THREE.Euler(Math.PI / 4, Math.PI / 8, 0)}
+      position={new THREE.Vector3(0.5, 0, -1)}
+    >
+      {/* <points
+        position={new THREE.Vector3(-1, 0, 0)}
+        rotation={new THREE.Euler(-Math.PI / 2, 0, 0)}
+        geometry={gridGeometry}
+        material={gridMaterial}
+      ></points> */}
+      {props.meshGroup.map((mesh) => {
+        const key = generateUUID();
+        const geometry = new THREE.BufferGeometry();
+
+        geometry.setAttribute("position", mesh.positions);
+        geometry.setAttribute("initialPosition", mesh.positions);
+        geometry.attributes.position.setUsage(THREE.DynamicDrawUsage);
+
+        const material = new THREE.PointsMaterial({
+          size: 0.02,
+          color: mesh.color,
+        });
+        return (
+          <points
+            key={key}
+            position={mesh.position}
+            scale={mesh.scale}
+            color={mesh.color}
+            geometry={geometry}
+            material={material}
+          ></points>
+        );
+      })}
+    </object3D>
+  );
+}
+
+function combineBuffer(model, bufferName) {
+  let count = 0;
+
+  model.traverse(function (child) {
+    if (child.isMesh) {
+      const buffer = child.geometry.attributes[bufferName];
+
+      count += buffer.array.length;
+    }
+  });
+
+  const combined = new Float32Array(count);
+
+  let offset = 0;
+
+  model.traverse(function (child) {
+    if (child.isMesh) {
+      const buffer = child.geometry.attributes[bufferName];
+
+      combined.set(buffer.array, offset);
+      offset += buffer.array.length;
+    }
+  });
+
+  return new THREE.BufferAttribute(combined, 3);
+}
+
+function createMesh(positions, scale, x, y, z, color) {
+  let mesh;
+  let meshGroup = [];
+  const clones = [
+    [-3, 0, -3],
+    [3, 0, 0],
+    [3, 0, 3],
+    [3, 0, -5],
+    [4, 0, 2],
+    [-4, 0, 4],
+    [-1, 0, -2],
+    [-3, 0, 3],
+  ];
+
+  const colors = [0xffdd44, 0xffffff, 0xff4422, 0xff9955, 0xff77dd, 0x252525];
+
+  for (let i = 0; i < clones.length; i++) {
+    const c = colors[(i + 3) % colors.length];
+    mesh = {
+      scale: new THREE.Vector3(scale, scale, scale),
+      position: new THREE.Vector3(
+        clones[i][0] + x,
+        clones[i][1] + y,
+        clones[i][2] + z
+      ),
+      color: c,
+      positions: positions.clone(),
+      initialPositions: positions.clone(),
+      direction: 0,
+      verticesDown: 0,
+      verticesUp: 0,
+      speed: 15,
+      delay: Math.floor(200 + 200 * Math.random()),
+    };
+    meshGroup.push(mesh);
+  }
+  return meshGroup;
+}
